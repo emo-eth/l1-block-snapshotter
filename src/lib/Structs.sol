ddc8b1c928139bedcf5596ce6b98b6c2646dd6e8
James Wenzel <jameswenzel@users.noreply.github.com>
2023-07-11 16:42:22 -0700

    pack uint64s

--- src/lib/Structs.sol
+++ src/lib/Structs.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.17;
 struct L1BlockSnapshot {
     uint64 number;
     uint64 timestamp;
+    uint64 sequenceNumber;
     uint256 basefee;
     bytes32 hash;
-    uint64 sequenceNumber;
     bytes32 batcherHash;
     uint256 l1FeeOverhead;
     uint256 l1FeeScalar;
